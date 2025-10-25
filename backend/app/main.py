a739c96ce198915eb678a2f9fe74de58582e0a51
clearpte1 <clearpte2025@gmail.com>
2025-10-25 13:50:14 +0530

    cors issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,12 +7,18 @@ from app.core import firebase
 
 app = FastAPI()
 
-origins = ["http://localhost", "http://localhost:3000", "https://clearpte.vercel.app"]
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "https://clearpte.vercel.app"
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=origins,        # allowed origins
+    allow_credentials=True,       # allow cookies/auth headers
+    allow_methods=["*"],          # allow all HTTP methods (GET, POST, OPTIONS, etc.)
+    allow_headers=["*"],          # allow all headers
 )
 
 app.include_router(api_router)
