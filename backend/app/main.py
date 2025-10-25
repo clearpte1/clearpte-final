e432064ee57a01c889fead564deb60afdeaf071c
clearpte1 <clearpte2025@gmail.com>
2025-10-25 13:33:22 +0530

    fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,19 +7,12 @@ from app.core import firebase
 
 app = FastAPI()
 
-origins = [
-    "https://clearpte.vercel.app",  # frontend
-    "https://clearpte-uuq9-6zpl2zmgw-reeteshs-projects-1e809ccb.vercel.app",  # backend
-    "http://localhost:3000",  # local React dev
-    "http://localhost"        # fallback
-]
+origins = ["http://localhost", "http://localhost:3000", "https://clearpte.vercel.app"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,        # only these origins allowed
-    allow_credentials=True,       # allow cookies / auth headers if needed
-    allow_methods=["*"],          # allow all HTTP methods (GET, POST, etc.)
-    allow_headers=["*"],          # allow all headers
+    allow_origins=origins,
+    allow_credentials=True,
 )
 
 app.include_router(api_router)
