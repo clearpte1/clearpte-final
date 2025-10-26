1b9d2f5f49f4b443b28209c24570659569b524d8
clearpte1 <clearpte2025@gmail.com>
2025-10-26 14:11:12 +0530

    test routes

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -6,10 +6,10 @@ from app.api import healthcheck, openai_routes, auth_routes, evaluation_routes, 
 api_router = APIRouter()
 
 api_router.include_router(healthcheck.router, prefix="/health", tags=["Healthcheck"])
-api_router.include_router(openai_routes.router, prefix="/api", tags=["Audio Comparison"])
+api_router.include_router(openai_routes.router, prefix="/compare", tags=["Audio Comparison"])
 api_router.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
-api_router.include_router(evaluation_routes.router, prefix="/api", tags=["Evaluation"])
-api_router.include_router(uploadquestions.router, prefix="/api", tags=["Upload Questions"])
-api_router.include_router(questions.router, prefix="/api/questions", tags=["Questions"])
+api_router.include_router(evaluation_routes.router, prefix="/evaluate", tags=["Evaluation"])
+api_router.include_router(uploadquestions.router, prefix="/upload", tags=["Upload Questions"])
+api_router.include_router(questions.router, prefix="/questions", tags=["Questions"])
 api_router.include_router(users.router, prefix="/users", tags=["Users"])
 
